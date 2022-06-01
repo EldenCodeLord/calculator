814d7e6052bf1d13d82d3850f9bed148f7c3f612
Manuel Raygoza <Manny.raygoza@gmail.com>
2022-05-31 23:46:25 -0700

    WIP: Added subtraction to calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,14 @@ function add(number1 , number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+const any = add(number1, number2);
+alert(result);
+
+function subtract(number3 , number4) {
+  return number1 - number2;
+}
+
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+const result = subtract(number3, number4);
 alert(result);
