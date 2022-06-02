74ce2c85899cbd763780ea934620afb8e04e4b15
MoseyByrne <mosey.b.byrne@gmail.com>
2022-06-02 14:02:15 -0700

    add user interface for subtract multiply and divide
    Co-authored-by: Manual Raygoza <manny.raygoza@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,55 @@
-// Business logics:
-
 function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
-
 function subtract(number1, number2) {
   return number1 - number2;
 }
-$(document).ready(function() {
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
-  const result = subtract(number1, number2);
-  alert(result);
-});
 
 function multiply(number1, number2) {
   return number1 * number2;
 }
-$(document).ready(function() {
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
-  const result = multiply(number1, number2);
-  alert(result);
-});
 
 function divide(number1, number2) {
   return number1 / number2;
 }
 
 $(document).ready(function() {
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
-  const result = divide(number1, number2);
-  alert(result);
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
 });
 
-
-// Interface Logics: 
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
+});
 
 $(document).ready(function() {
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
-  const result = add(number1, number2);
-  alert(result);
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
+    $("#output4").text(result);
+  });
 });
